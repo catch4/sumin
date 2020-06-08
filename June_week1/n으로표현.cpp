7141dbfcaa83d4c1db04b6a8b6136480fc50318c
ttumzzi <ssumini0829@gmail.com>
2020-06-08 17:44:59 +0900

    unsolved(0): #42895

--- June_week1/n으로표현.cpp
+++ June_week1/n으로표현.cpp
@@ -19,13 +19,13 @@ int solution(int N, int number) {
     for (int i = 1; i <= 8; i++)
         v.push_back({getContinuousNum(N, i)});
 
-    for (int i = 0; i < 8; i++) {
+    for (int i = 1; i < 8; i++) {
         for (int j = 0; j < i; j++) {
             for (const auto& num1 : v[j]) {
                 for (const auto& num2 : v[i - j - 1]) {
                     v[i].insert(num1 + num2);
-                    v[i].insert(num1 + num2);
-                    v[i].insert(num1 + num2);
+                    v[i].insert(num1 - num2);
+                    v[i].insert(num1 * num2);
                     if (num2 != 0) v[i].insert(num1 / num2);
                 }
             }
