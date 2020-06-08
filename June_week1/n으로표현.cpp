6ae5b73f581e2a2b68a45eec4857448b40122b87
ttumzzi <ssumini0829@gmail.com>
2020-06-08 17:44:03 +0900

    unsolved(0): #42895

---
+++ June_week1/n으로표현.cpp
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<set<int>> v;
+
+int getContinuousNum(int n, int repeat) {
+    string res = "";
+    for (int i = 0; i < repeat; i++) res += (n + '0');
+    return atoi(res.c_str());
+}
+
+int solution(int N, int number) {
+    int answer = -1;
+    for (int i = 1; i <= 8; i++)
+        v.push_back({getContinuousNum(N, i)});
+
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < i; j++) {
+            for (const auto& num1 : v[j]) {
+                for (const auto& num2 : v[i - j - 1]) {
+                    v[i].insert(num1 + num2);
+                    v[i].insert(num1 + num2);
+                    v[i].insert(num1 + num2);
+                    if (num2 != 0) v[i].insert(num1 / num2);
+                }
+            }
+        }
+        if (v[i].find(number) != v[i].end()) {
+            answer = i + 1;
+            break;
+        }
+    }
+    return answer;
+}
+
+int main() {
+    cout << solution(2, 11);
+}
