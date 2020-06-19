29cb0911fd3750de380cb20e303b9ae587941d44
ttumzzi <ssumini0829@gmail.com>
2020-06-19 12:16:08 +0900

    solve: PRO # 42895 n으로 표현

--- June_week1/n으로표현.cpp
+++ June_week1/n으로표현.cpp
@@ -1,4 +1,6 @@
-#include <stdlib.h>
+// PRO # 42895 n으로 표현
+// 아이디어를 생각하는게 어려웠지, 구현은 어렵지 않았다.
+// 비슷한 유형의 문제가 나오면 떠올려서 풀면 좋을 듯
 
 #include <iostream>
 #include <set>
@@ -6,36 +8,40 @@
 #include <vector>
 using namespace std;
 
-vector<set<int>> v;
+set<int> arr[9];
 
-int getContinuousNum(int n, int repeat) {
-    string res = "";
-    for (int i = 0; i < repeat; i++) res += (n + '0');
-    return atoi(res.c_str());
+int makeNum(int N, int iter) {
+    int res = 0;
+    for (int i = 0; i < iter; i++) {
+        res += N;
+        N *= 10;
+    }
+    return res;
 }
 
 int solution(int N, int number) {
-    int answer = -1;
-    for (int i = 1; i <= 8; i++)
-        v.push_back({getContinuousNum(N, i)});
-
-    for (int i = 1; i < 8; i++) {
-        for (int j = 0; j < i; j++) {
-            for (const auto& num1 : v[j]) {
-                for (const auto& num2 : v[i - j - 1]) {
-                    v[i].insert(num1 + num2);
-                    v[i].insert(num1 - num2);
-                    v[i].insert(num1 * num2);
-                    if (num2 != 0) v[i].insert(num1 / num2);
+    if (N == number) return 1;
+    for (int i = 1; i <= 8; i++) {
+        arr[i].insert(makeNum(N, i));
+        for (int j = 1; j <= i / 2; j++) {
+            int a = j, b = i - j;
+            for (auto it1 = arr[a].begin(); it1 != arr[a].end(); ++it1) {
+                for (auto it2 = arr[b].begin(); it2 != arr[b].end(); ++it2) {
+                    int num1 = *it1, num2 = *it2;
+                    arr[i].insert(num1 + num2);
+                    arr[i].insert(num1 - num2);
+                    arr[i].insert(num2 - num1);
+                    arr[i].insert(num1 * num2);
+                    if (num2 != 0) arr[i].insert(num1 / num2);
+                    if (num1 != 0) arr[i].insert(num2 / num1);
                 }
             }
         }
-        if (v[i].find(number) != v[i].end()) {
-            answer = i + 1;
-            break;
+        for (auto it = arr[i].begin(); it != arr[i].end(); it++) {
+            if (*it == number) return i;
         }
     }
-    return answer;
+    return -1;
 }
 
 int main() {
